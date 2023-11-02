0a462983fc92e599245da24eab75a140b7cb0e86
shangxin <1083732908@qq.com>
2023-11-02 19:04:14 +0800

    fix: isEmptyFn 判断比较耗性能

--- src/plugins/keyup/index.ts
+++ src/plugins/keyup/index.ts
@@ -7,7 +7,10 @@ export const createKeyUpPlugin = createPluginFactory({
     onKeyUp: (editor) => (event: any) => {
       // editorIsEmpty(editor) 用来防止没内容删除的时候placeholder 抖动
       // 判断内容是空的话删除内容，有一个spanElement placeholder判定不显示
-      if (isEmptyFn(editor) && isDeleteKey(event) && !editorIsEmpty(editor)) {
+      if (!isDeleteKey(event)) return;
+      if (editorIsEmpty(editor)) return;
+      if (isEmptyFn(editor)) {
+        console.log('清除编辑器内容----');
         editor?.clear();
       }
     },
